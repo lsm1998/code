b4471d44d7e8a98118aaf7e1a4b2e11a00b19a4f
lsm1998 <487005831@qq.com>
2019-06-21 17:13:49 +0800

    Courage项目主方法

---
+++ Courage/src/main/java/com/lsm1998/courage/CourageApplication.java
@@ -0,0 +1,14 @@
+package com.lsm1998.courage;
+
+/**
+ * @作者：刘时明
+ * @时间：2019/6/21-17:12
+ * @作用：Courage语言解释器入口
+ */
+public class CourageApplication
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
